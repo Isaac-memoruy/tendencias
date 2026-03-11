033c23041f87ed31934a58f4c77f8e0be82a6590
jesus <jesustorres200503@gmail.com>
2026-03-11 12:00:59 -0500

    isacc

---
+++ laravel/laravel/proyecto/resources/views/suma.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Sumar dos numeros </h1>
+    <form action="/sumar" method="post">
+        @csrf
+        <label for="num1">Numero 1:</label>
+        
+
+    </form>
+</body>
+</html>
