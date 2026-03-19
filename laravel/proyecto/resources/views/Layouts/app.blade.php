67dbc8165d0c9b89847e5cd7c86fcd75e59d29cb
isaac+ <isaacdavidgarciavesga@gmail.com>
2026-03-19 15:38:00 -0500

    a

---
+++ laravel/proyecto/resources/views/Layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet"
+    href = "http://cdsn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css"
+    integrity = "sha384-+0n0xVw2eSRO3z">
+</head>
+<body>
+    <a href="#">Bienvenido</a>
+    <a href="#">Suma</a>
+    <a href="#">Opcion 3</a>
+    @yield('content')
+    Hola soy el template de la aplicacion
+
+</body>
+</html>
--- laravel/proyecto/resources/views/suma.blade.php
+++ laravel/proyecto/resources/views/suma.blade.php
@@ -1,31 +1,19 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Suma</title>
-</head>
-<body>
+@extends('layouts.app')
+@section('content')
 
-<h1>Sumar dos numeros</h1>
-
-<form action="/suma" method="POST">
-    @csrf
-
-    <label>Numero 1:</label>
-    <input type="number" name="num1" required>
-
-    <br><br>
-
-    <label>Numero 2:</label>
-    <input type="number" name="num2" required>
-
-    <br><br>
-
-    <button type="submit">Sumar</button>
-
-</form>
-
-<br>
-
-Resultado: {{$resultado}}
-</body>
-</html>
+        <h1 class="text-center">Sumar dos números</h1>
+        <form action="/suma" method="POST">
+            @csrf
+            <label>Número 1:</label>
+            <input type="number" name="num1" required>
+            <br><br>
+            <label>Número 2:</label>
+            <input type="number" name="num2" required>
+            <br><br>
+            <button type="submit">Sumar</button>
+        </form>
+    <br>
+        @if(isset($resultado))
+            <h2>Resultado: {{$resultado}}</h2>
+        @endif
+@endsection
